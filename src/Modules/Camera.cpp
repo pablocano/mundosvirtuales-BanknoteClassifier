375dcddb1a923a0bade0938592ad090401f607ef
Pablo Cano <pcano@die.uchile.cl>
2016-05-24 16:39:44 -0400

    Se arreglo la ruta del archivo cubo.txt para OSX

--- src/Modules/Camera.cpp
+++ src/Modules/Camera.cpp
@@ -10,8 +10,10 @@ Camera::Camera(): video0(0),video1(1),index(0),
     camerasInfo[0] = &upper;
     camerasInfo[1] = &lower;
     last = std::chrono::steady_clock::now();
-    video0.set(CV_CAP_PROP_FRAME_HEIGHT, 1080);
-    video0.set(CV_CAP_PROP_FRAME_WIDTH, 1920);
+    video0.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+    video0.set(CV_CAP_PROP_FRAME_WIDTH, 620);
+    video1.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+    video1.set(CV_CAP_PROP_FRAME_WIDTH, 620);
 }
 
 void Camera::update(FrameInfo *frameInfo)
