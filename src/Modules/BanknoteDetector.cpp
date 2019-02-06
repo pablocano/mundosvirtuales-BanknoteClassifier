bc017e5d6c57ac830db1fc339827f11e7282a02d
Kenzo <kenzo.lobos@gmail.com>
2019-02-05 23:34:27 -0300

    Refactor. Still works

--- src/Modules/BanknoteDetector.cpp
+++ src/Modules/BanknoteDetector.cpp
@@ -15,39 +15,31 @@
 
 MAKE_MODULE(BanknoteDetector, BanknoteClassifier)
 
+ClassDetections::ClassDetections()
+{
+    matches.reserve(10000);
+    houghFilteredMatches.reserve(1000);
+}
+
 BanknoteDetector::BanknoteDetector():
-    trainBanknoteHeight(200),
-    resizeModels(false)
+    resizeModels(false),
+    trainBanknoteHeight(200)
+
 {
     OUTPUT_TEXT("wololo init");
 
     /* Initialize CUDA + SURF + Matcher */
     matcher = cv::cuda::DescriptorMatcher::createBFMatcher();
-    surf = cv::cuda::SURF_CUDA(400, 4, 4, true);
+    surf = cv::cuda::SURF_CUDA(100, 4, 4, true);
     clahe = cv::createCLAHE(2.0, cv::Size(7, 7));
 
-    modelImages.resize(Classification::numOfBanknotes - 2);
-    modelGpuImages.resize(Classification::numOfBanknotes - 2);
-    modelFeatures.resize(Classification::numOfBanknotes - 2);
-    modelMasks.resize(Classification::numOfBanknotes - 2);
     imageKeypoints.resize(10000);
 
-    matches.resize(Classification::numOfBanknotes - 2);
-    filteredMatches.resize(Classification::numOfBanknotes - 2);
-    houghFilteredMatches.resize(Classification::numOfBanknotes - 2);
-
-    for(unsigned i = 0; i < Classification::numOfBanknotes - 2; i++)
+    for(unsigned c = 0; c < Classification::numOfBanknotes - 2; c++)
     {
-        matches[i].reserve(50000);
-        filteredMatches[i].reserve(50000);
-        houghFilteredMatches[i].reserve(50000);
-    }
-
-    for(unsigned i = 0; i < Classification::numOfBanknotes - 2; i++)
-    {
-        // Read the image and resize it
-        cv::Mat image = cv::imread(std::string(File::getGTDir()) + "/Data/img_real/" + Classification::getName((Classification::Banknote)i) + ".jpg", cv::IMREAD_GRAYSCALE);
-        cv::Mat maskGrayscale = cv::imread(std::string(File::getGTDir()) + "/Data/img_real/" + Classification::getName((Classification::Banknote)i) + "_mask.jpg", cv::IMREAD_GRAYSCALE);
+        // Load models
+        cv::Mat image = cv::imread(std::string(File::getGTDir()) + "/Data/img_real/" + Classification::getName((Classification::Banknote) c) + ".jpg", cv::IMREAD_GRAYSCALE);
+        cv::Mat maskGrayscale = cv::imread(std::string(File::getGTDir()) + "/Data/img_real/" + Classification::getName((Classification::Banknote) c) + "_mask.jpg", cv::IMREAD_GRAYSCALE);
 
         cv::Mat binaryMask(maskGrayscale.size(), CV_8U);
         cv::threshold(maskGrayscale, binaryMask, 127, 255, cv::THRESH_BINARY);
@@ -58,25 +50,22 @@ BanknoteDetector::BanknoteDetector():
         // Calculate the features of the image
         Features f;
 
-        cv::cuda::GpuMat imageGpu;
-        imageGpu.upload(image);
-        surf(imageGpu,cv::cuda::GpuMat(),f.keypointsGpu[0],f.descriptors[0]);
-        surf.downloadKeypoints(f.keypointsGpu[0],f.keypoints[0]);
+        cv::cuda::GpuMat gpuImage;
+        gpuImage.upload(image);
+        surf(gpuImage, cv::cuda::GpuMat(), f.keypointsGpu[0], f.descriptors[0]);
+        surf.downloadKeypoints(f.keypointsGpu[0], f.keypoints[0]);
 
-        modelFeatures[i] = f;
-        modelGpuImages[i] = imageGpu;
-        modelImages[i] = image;
-        modelMasks[i] = binaryMask;
+        Model& model = models[c];
+        model.features = f;
+        model.gpuImage = gpuImage;
+        model.image = image;
+        model.mask = binaryMask;
 
-        std::vector<Eigen::Vector3f> aux_corners;
-        aux_corners.push_back(Eigen::Vector3f(0,0,1));
-        aux_corners.push_back(Eigen::Vector3f(image.cols,0,1));
-        aux_corners.push_back(Eigen::Vector3f(image.cols, image.rows,1));
-        aux_corners.push_back(Eigen::Vector3f(0, image.rows,1));
-        //aux_corners.push_back(Eigen::Vector3f(image.cols/2,trainBanknoteHeight/2.0,1));
-        //aux_corners.push_back(Eigen::Vector3f(image.cols,trainBanknoteHeight/2.0,1));
-
-        modelCorners.push_back(aux_corners);
+        model.corners.clear();
+        model.corners.push_back(Eigen::Vector3f(0,0,1));
+        model.corners.push_back(Eigen::Vector3f(image.cols,0,1));
+        model.corners.push_back(Eigen::Vector3f(image.cols, image.rows,1));
+        model.corners.push_back(Eigen::Vector3f(0, image.rows,1));
     }
 
     debugColors[Classification::UNO_C] = ColorRGBA::green;
@@ -107,7 +96,7 @@ void BanknoteDetector::update(BanknoteDetections& detections)
 
     auto start = std::chrono::system_clock::now();
 
-    GpuImage.upload(theGrayScaleImageEq);
+    gpuImage.upload(theGrayScaleImage);
 
     auto end = std::chrono::system_clock::now();
 
@@ -115,7 +104,7 @@ void BanknoteDetector::update(BanknoteDetections& detections)
 
     start = end;
 
-    surf(GpuImage, GpuImageMask, GpuImageKeypoints, GpuImageDescriptors);
+    surf(gpuImage, gpuImageMask, gpuImageKeypoints, gpuImageDescriptors);
 
     end = std::chrono::system_clock::now();
 
@@ -123,151 +112,45 @@ void BanknoteDetector::update(BanknoteDetections& detections)
 
     start = end;
 
-    surf.downloadKeypoints(GpuImageKeypoints, imageKeypoints);
+    surf.downloadKeypoints(gpuImageKeypoints, imageKeypoints);
 
     end = std::chrono::system_clock::now();
 
     std::cout << "Download time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
 
-    for(const auto& kp : imageKeypoints)
-    {
-        CIRCLE("module:BanknoteDetections:filtered", kp.pt.x, kp.pt.y, 5, 1, Drawings::ps_solid, ColorRGBA(255,255,255,32), Drawings::ps_solid, ColorRGBA(255,255,255,32));
-    }
-
     start = end;
 
     int numberOfMatches = 0;
 
-    for(unsigned i = 0; i < Classification::numOfBanknotes - 2; i++)
+    for(unsigned c = 0; c < Classification::numOfBanknotes - 2; c++)
     {
-        //matcher->knnMatch(GpuImageDescriptors, modelFeatures[i].descriptors[0], matches[i], 2);
-        matcher->match(GpuImageDescriptors, modelFeatures[i].descriptors[0], filteredMatches[i]);
-        numberOfMatches += matches[i].size();
+        Model& model = models[c];
+        ClassDetections& detections = classDetections[c];
+
+        matcher->match(gpuImageDescriptors, model.features.descriptors[0], detections.matches);
+        numberOfMatches += detections.matches.size();
     }
 
     end = std::chrono::system_clock::now();
     std::cout << "KNN Matches time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms (Matches: " << numberOfMatches << ")" << std::endl;
 
     start = end;
-
-    numberOfMatches = 0;
-    for(unsigned c = 0; c < Classification::numOfBanknotes - 2; c++)
-    {
-        break;
-        const std::vector<std::vector<cv::DMatch>>& rawClassMatches = matches[c];
-        std::vector<cv::DMatch>& filteredClassMatches = filteredMatches[c];
-        filteredClassMatches.clear();
-
-        for(std::vector<cv::DMatch> match : rawClassMatches)
-        {
-            bool dist_crit = match[0].distance < 0.8f * match[1].distance;
-            float scale = imageKeypoints[match[0].queryIdx].size / modelFeatures[c].keypoints[0][match[0].trainIdx].size;
-
-            if(dist_crit && scale > 0.8f && scale < 1.2f)
-            {
-                filteredClassMatches.push_back(match[0]);
-            }
-        }
-
-        numberOfMatches += filteredClassMatches.size();
-    }
-
-    end = std::chrono::system_clock::now();
-    std::cout << "Initial Matches filter time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms (Matches: " << numberOfMatches << ")" << std::endl;
-
-    /** Basic Filtered Drawings */
-    /*for(unsigned c = 0; c < Classification::numOfBanknotes - 2; c++)
-    {
-        cv::Mat img_accepted;
-        cv::drawMatches(theGrayScaleImageEq, imageKeypoints, modelImages[c], modelFeatures[c].keypoints[0], filteredMatches[c], img_accepted);
-        imwrite("basic_" + std::to_string(c) + ".jpg", img_accepted);
-
-        ColorRGBA color = debugColors[c];
-        const std::vector<Eigen::Vector3f>& corners = modelCorners[c];
-
-        for(const cv::DMatch& match : filteredMatches[c])
-        {
-            const cv::KeyPoint& queryKeypoint = imageKeypoints[match.queryIdx];
-            const cv::KeyPoint& trainKeypoint = modelFeatures[c].keypoints[0][match.trainIdx];
-
-            CIRCLE("module:BanknoteDetections:raw_keypoints", imageKeypoints[match.queryIdx].pt.x, imageKeypoints[match.queryIdx].pt.y, 5, 1, Drawings::ps_solid, color, Drawings::ps_solid, color);
-
-            cv::Mat transform = getTransformAsMat(trainKeypoint, queryKeypoint);
-            Eigen::Map<Eigen::Matrix<float, 3, 3, Eigen::RowMajor>> transform2(transform.ptr<float>());
-            Eigen::Matrix3f transform3 = transform2;
-            std::vector<Eigen::Vector3f> corners2 = corners;
-
-            for(Eigen::Vector3f& corner : corners2)
-                corner = transform2 * corner;
-
-            for(int i = 0; i < corners2.size(); i++)
-            {
-                corners2[i] = transform3 * corners[i];
-            }
-
-            for(int i = 0; i < corners2.size() - 1; i++)
-            {
-                LINE("module:BanknoteDetections:raw_detections", corners2[i].x(), corners2[i].y() , corners2[i + 1].x(), corners2[i + 1].y(), 3, Drawings::dot, color);
-            }
-            LINE("module:BanknoteDetections:raw_detections", corners2.front().x(), corners2.front().y() , corners2.back().x(), corners2.back().y(), 3, Drawings::dot, color);
-        }
-    }*/
-
-    start = std::chrono::system_clock::now();
     numberOfMatches = 0;
 
     for(unsigned c = 0; c < Classification::numOfBanknotes - 2; c++)
     {
-        houghFilteredMatches[c].clear();
-        hough4d(filteredMatches[c], imageKeypoints, modelFeatures[c].keypoints[0], modelMasks[c], houghFilteredMatches[c]);
-        numberOfMatches += houghFilteredMatches[c].size();
+        Model& model = models[c];
+        ClassDetections& detections = classDetections[c];
+
+        detections.houghFilteredMatches.clear();
+        hough4d(model, detections);
+        numberOfMatches += detections.houghFilteredMatches.size();
     }
 
     end = std::chrono::system_clock::now();
     std::cout << "Hough Matches filter time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms (Matches: " << numberOfMatches << ")" << std::endl;
 
-    /** Basic Filtered Drawings */
-    for(unsigned c = 0; c < Classification::numOfBanknotes - 2; c++)
-    {
-        cv::Mat img_accepted;
-        cv::drawMatches(theGrayScaleImageEq, imageKeypoints, modelImages[c], modelFeatures[c].keypoints[0], houghFilteredMatches[c], img_accepted);
-        imwrite("hough_" + std::to_string(c) + ".jpg", img_accepted);
-
-        ColorRGBA color = debugColors[c];
-        const std::vector<Eigen::Vector3f>& corners = modelCorners[c];
-
-        for(const cv::DMatch& match : houghFilteredMatches[c])
-        {
-            const cv::KeyPoint& queryKeypoint = imageKeypoints[match.queryIdx];
-            const cv::KeyPoint& trainKeypoint = modelFeatures[c].keypoints[0][match.trainIdx];
-
-            CIRCLE("module:BanknoteDetections:hough_keypoints", queryKeypoint.pt.x, queryKeypoint.pt.y, 5, 1, Drawings::ps_solid, color, Drawings::ps_solid, color);
-
-            cv::Mat transform = getTransformAsMat(trainKeypoint, queryKeypoint);
-            Eigen::Map<Eigen::Matrix<float, 3, 3, Eigen::RowMajor>> transform2(transform.ptr<float>());
-            Eigen::Matrix3f transform3 = transform2;
-            std::vector<Eigen::Vector3f> corners2 = corners;
-
-            //for(Eigen::Vector3f& corner : corners2)
-            //    corner = transform2 * corner;
-
-            Eigen::Vector3f p = Eigen::Vector3f(trainKeypoint.pt.x, trainKeypoint.pt.y, 1.f);
-            Eigen::Vector3f p2 = transform3 * p;
-            CIRCLE("module:BanknoteDetections:hough_keypoints", p2.x(), p2.y(), 5, 1, Drawings::ps_solid, color, Drawings::ps_solid, ColorRGBA::white);
-
-            for(int i = 0; i < corners2.size(); i++)
-            {
-                corners2[i] = transform3 * corners[i];
-            }
-
-            for(int i = 0; i < corners2.size() - 1; i++)
-            {
-                LINE("module:BanknoteDetections:hough_detections", corners2[i].x(), corners2[i].y() , corners2[i + 1].x(), corners2[i + 1].y(), 3, Drawings::dot, color);
-            }
-            LINE("module:BanknoteDetections:hough_detections", corners2.front().x(), corners2.front().y() , corners2.back().x(), corners2.back().y(), 3, Drawings::dot, color);
-        }
-    }
-
+    drawAcceptedHough();
 }
 
 
@@ -319,92 +202,130 @@ void BanknoteDetector::getTransform(const cv::KeyPoint& src, const cv::KeyPoint&
 }
 
 // Hough Based Matching
-void BanknoteDetector::hough4d(
-  std::vector<cv::DMatch>& matches,
-  std::vector<cv::KeyPoint>& imageKeypoints,
-  std::vector<cv::KeyPoint>& modelKeypoints,
-  const cv::Mat& mask,
-  std::vector<cv::DMatch>& accepted)
+void BanknoteDetector::hough4d(const Model& model, ClassDetections& detections)
 {
-  int hsize[] = {1000, 1000, 1000, 1000};
-  cv::SparseMat sm(4, hsize, CV_32F);
-  int maxVotes = 0;
+    /* Handy references */
+    const std::vector<cv::DMatch> matches = detections.matches;
+    const std::vector<cv::KeyPoint> modelKeypoints = model.features.keypoints[0];
 
-  // Hough Parameters
-  double dxBin   = 30; // 60 pixels
-  double dangBin = 30; // 30 degrees
-  int votesTresh = 10;
+    int maxVotes = 0;
 
-  for (int index = 0; index < matches.size(); index++)
-  {
-    float e, theta, tx, ty;
-    const cv::DMatch& match = matches[index];
-    const cv::KeyPoint& imageKeypoint = imageKeypoints[match.queryIdx];
-    const cv::KeyPoint& modelKeypoint = modelKeypoints[match.trainIdx];
+    // Hough Parameters
+    double dxBin   = 30; // 60 pixels
+    double dangBin = 30; // 30 degrees
+    int votesTresh = 10;
 
-    getTransform(modelKeypoint, imageKeypoint, tx, ty, theta, e);
+    int hsize[] = {1000, 1000, 1000, 1000};
+    cv::SparseMat sm(4, hsize, CV_32F);
 
-    if(e < 0.5 || e > 2.0)
-        continue;
+    for (int index = 0; index < matches.size(); index++)
+    {
+        float e, theta, tx, ty;
+        const cv::DMatch& match = matches[index];
+        const cv::KeyPoint& imageKeypoint = imageKeypoints[match.queryIdx];
+        const cv::KeyPoint& modelKeypoint = modelKeypoints[match.trainIdx];
 
-    int ptx = (int) modelKeypoint.pt.x;
-    int pty = (int) modelKeypoint.pt.y;
+        getTransform(modelKeypoint, imageKeypoint, tx, ty, theta, e);
 
-    if(mask.at<unsigned char>(pty,ptx) == 0)
-        continue;
+        if(e < 0.5 || e > 2.0)
+            continue;
 
-    int i = floor(tx / dxBin + 0.5);
-    int j = floor(ty / dxBin + 0.5);
-    int k = floor(theta / dangBin + 0.5);
-    int z = floor(log(e) / log(2.0) + 0.5);
+        int ptx = (int) modelKeypoint.pt.x;
+        int pty = (int) modelKeypoint.pt.y;
 
-    int idx[4];
-    idx[0] = i + 500;
-    idx[1] = j + 500;
-    idx[2] = k + 500;
-    idx[3] = z + 500;
+        if(model.mask.at<unsigned char>(pty, ptx) == 0)
+            continue;
 
-    //assert(idx[0] >= 0 && idx[0] < 1000);
-    //assert(idx[1] >= 0 && idx[1] < 1000);
-    //assert(idx[2] >= 0 && idx[2] < 1000);
-    //assert(idx[3] >= 0 && idx[3] < 1000);
-    for (int u = 0; u < 4; u++)
-      if (idx[u] < 0 || idx[u] >= 1000)
-        continue;
+        int i = floor(tx / dxBin + 0.5);
+        int j = floor(ty / dxBin + 0.5);
+        int k = floor(theta / dangBin + 0.5);
+        int z = floor(log(e) / log(2.0) + 0.5);
 
-    sm.ref<float>(idx)++;
-    if (sm.ref<float>(idx) > maxVotes)
-      maxVotes = sm.ref<float>(idx);
+        int idx[4];
+        idx[0] = i + 500;
+        idx[1] = j + 500;
+        idx[2] = k + 500;
+        idx[3] = z + 500;
 
-  }
+        assert(idx[0] >= 0 && idx[0] < 1000);
+        assert(idx[1] >= 0 && idx[1] < 1000);
+        assert(idx[2] >= 0 && idx[2] < 1000);
+        assert(idx[3] >= 0 && idx[3] < 1000);
+        //for (int u = 0; u < 4; u++)
+        //    if (idx[u] < 0 || idx[u] >= 1000)
+        //        continue;
 
-  /*cout << "Hough: Max Votes=" << maxVotes << endl;*/
+        sm.ref<float>(idx)++;
+    }
 
-  for (int index = 0; index < matches.size(); index++)
-  {
-    float e, theta, tx, ty;
-    const cv::DMatch& match = matches[index];
-    const cv::KeyPoint& imageKeypoint = imageKeypoints[match.queryIdx];
-    const cv::KeyPoint& modelKeypoint = modelKeypoints[match.trainIdx];
+    for (int index = 0; index < matches.size(); index++)
+    {
+        float e, theta, tx, ty;
+        const cv::DMatch& match = matches[index];
+        const cv::KeyPoint& imageKeypoint = imageKeypoints[match.queryIdx];
+        const cv::KeyPoint& modelKeypoint = modelKeypoints[match.trainIdx];
 
-    getTransform(modelKeypoint, imageKeypoint, tx, ty, theta, e);
+        getTransform(modelKeypoint, imageKeypoint, tx, ty, theta, e);
 
-    if(e < 0.5 || e > 2.0)
-        continue;
+        if(e < 0.5 || e > 2.0)
+            continue;
 
-    int i = floor(tx / dxBin + 0.5);
-    int j = floor(ty / dxBin + 0.5);
-    int k = floor(theta / dangBin + 0.5);
-    int z = floor(log(e) / log(2.0) + 0.5);
+        int i = floor(tx / dxBin + 0.5);
+        int j = floor(ty / dxBin + 0.5);
+        int k = floor(theta / dangBin + 0.5);
+        int z = floor(log(e) / log(2.0) + 0.5);
 
-    int idx[4];
-    idx[0] = i + 500;
-    idx[1] = j + 500;
-    idx[2] = k + 500;
-    idx[3] = z + 500;
+        int idx[4];
+        idx[0] = i + 500;
+        idx[1] = j + 500;
+        idx[2] = k + 500;
+        idx[3] = z + 500;
 
-    if (sm.ref<float>(idx) >= votesTresh)
-      accepted.push_back(matches[index]);
-  }
+        if (sm.ref<float>(idx) >= votesTresh)
+            detections.houghFilteredMatches.push_back(matches[index]);
+    }
 
+}
+
+void BanknoteDetector::drawAcceptedHough()
+{
+    /** Hough Filtered Drawings - Optional */
+    for(unsigned c = 0; c < Classification::numOfBanknotes - 2; c++)
+    {
+        Model& model = models[c];
+        ClassDetections& detections = classDetections[c];
+
+        cv::Mat img_accepted;
+        cv::drawMatches(theGrayScaleImageEq, imageKeypoints, model.image, model.features.keypoints[0], detections.houghFilteredMatches, img_accepted);
+
+        ColorRGBA color = debugColors[c];
+        const std::vector<Eigen::Vector3f>& corners = model.corners;
+
+        for(const cv::DMatch& match : detections.houghFilteredMatches)
+        {
+            const cv::KeyPoint& queryKeypoint = imageKeypoints[match.queryIdx];
+            const cv::KeyPoint& trainKeypoint = model.features.keypoints[0][match.trainIdx];
+
+            CIRCLE("module:BanknoteDetections:hough_keypoints", queryKeypoint.pt.x, queryKeypoint.pt.y, 5, 1, Drawings::ps_solid, color, Drawings::ps_solid, color);
+
+            cv::Mat transform = getTransformAsMat(trainKeypoint, queryKeypoint);
+            Eigen::Map<Eigen::Matrix<float, 3, 3, Eigen::RowMajor>> transform2(transform.ptr<float>());
+            std::vector<Eigen::Vector3f> corners2 = corners;
+
+            for(int i = 0; i < corners2.size(); i++)
+            {
+                corners2[i] = transform2 * corners[i];
+            }
+
+            for(int i = 0; i < corners2.size() - 1; i++)
+            {
+                LINE("module:BanknoteDetections:hough_detections", corners2[i].x(), corners2[i].y() , corners2[i + 1].x(), corners2[i + 1].y(), 3, Drawings::dot, color);
+                cv::line (img_accepted, cv::Point(corners2[i].x(), corners2[i].y()), cv::Point(corners2[i + 1].x(), corners2[i + 1].y()), 255);
+            }
+            LINE("module:BanknoteDetections:hough_detections", corners2.front().x(), corners2.front().y() , corners2.back().x(), corners2.back().y(), 3, Drawings::dot, color);
+            cv::line (img_accepted, cv::Point(corners2.front().x(), corners2.front().y()), cv::Point(corners2.back().x(), corners2.back().y()), 255);
+        }
+
+        imwrite("hough_" + std::to_string(c) + ".jpg", img_accepted);
+    }
 }
--- src/Modules/BanknoteDetector.h
+++ src/Modules/BanknoteDetector.h
@@ -28,10 +28,47 @@
 
 MODULE(BanknoteDetector,
 {,
+    REQUIRES(GrayScaleImage),
     REQUIRES(GrayScaleImageEq),
     REQUIRES(FrameInfo),
     PROVIDES(BanknoteDetections),
 });
+
+class Model
+{
+public:
+    cv::cuda::GpuMat gpuImage;
+    cv::Mat image;
+    cv::Mat mask;
+    Features features;
+    std::vector<Eigen::Vector3f> corners;
+};
+
+class Hypothesys
+{
+public:
+
+    std::vector<cv::DMatch> matches;
+    Eigen::Matrix3f transform; /* From the model (a.k.a train image) to the camera image (a.k.a query image) */
+    Eigen::Matrix3f pose;
+    Eigen::Matrix3f graspPose;
+    float houghVotes;
+
+};
+
+class ClassDetections
+{
+  public:
+
+    ClassDetections();
+    void clear();
+
+    std::vector<cv::DMatch> matches;
+    std::vector<cv::DMatch> houghFilteredMatches;
+    std::vector<Hypothesys> hypotheses;
+};
+
+
 
 class BanknoteDetector : public BanknoteDetectorBase
 {
@@ -45,39 +82,32 @@ protected:
     void resizeImage(cv::Mat& image);
     cv::Mat getTransformAsMat(const cv::KeyPoint& src, const cv::KeyPoint& dst);
     inline void getTransform(const cv::KeyPoint& src, const cv::KeyPoint& dst, float& tx, float& ty, float& angleDegrees, float& e);
-    void hough4d(std::vector<cv::DMatch>& matches, std::vector<cv::KeyPoint>& imageKeypoints, std::vector<cv::KeyPoint>& modelKeypoints, const cv::Mat& mask, std::vector<cv::DMatch>& accepted);
+    void hough4d(const Model& model, ClassDetections& detections);
+
+    void drawAcceptedHough();
 
     cv::Ptr<cv::cuda::DescriptorMatcher> matcher;
     cv::cuda::SURF_CUDA surf;
     cv::Ptr<cv::CLAHE> clahe;
 
     /** Gpu Buffers */
-    cv::cuda::GpuMat GpuImage;
-    cv::cuda::GpuMat GpuImageKeypoints;
-    cv::cuda::GpuMat GpuImageDescriptors;
-    cv::cuda::GpuMat GpuImageMask;
+    cv::cuda::GpuMat gpuImage;
+    cv::cuda::GpuMat gpuImageKeypoints;
+    cv::cuda::GpuMat gpuImageDescriptors;
+    cv::cuda::GpuMat gpuImageMask;
 
     /** RAM Buffers */
     std::vector<cv::KeyPoint> imageKeypoints;
 
+    /** Models */
+    Model models[Classification::numOfBanknotes - 2];
+
     /** Matches Buffer. Should really use an alias or somethin'. Shape is classes x matches x 2 (2 is the KNN criteria)*/
-    std::vector<std::vector<std::vector<cv::DMatch>>> matches;
-    std::vector<std::vector<cv::DMatch>> filteredMatches;
-    std::vector<std::vector<cv::DMatch>> houghFilteredMatches;
-
-    /** Hardcoded parameters */
-
+    ClassDetections classDetections[Classification::numOfBanknotes - 2];
 
     /** Module Parameters */
-    bool resizeModels;
+    bool resizeModels; /* This is a must when using scanned images */
     int trainBanknoteHeight; /* hardcoded parameter in order to resize*/
-
-    /** Model Buffers */
-    std::vector<cv::cuda::GpuMat> modelGpuImages;
-    std::vector<cv::Mat> modelImages;
-    std::vector<cv::Mat> modelMasks;
-    std::vector<Features> modelFeatures;
-    std::vector<std::vector<Eigen::Vector3f>> modelCorners;
 
     ColorRGBA debugColors[Classification::numOfBanknotes - 2];
 };
